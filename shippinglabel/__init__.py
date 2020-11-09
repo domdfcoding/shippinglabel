f9c8910b437b664fd2c5c2ad322ea22e476489ff
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-09 13:31:34 +0000

    Bump version v0.1.0 -> v0.1.1

--- shippinglabel/__init__.py
+++ shippinglabel/__init__.py
@@ -35,7 +35,7 @@ __all__ = ["no_dev_versions", "normalize"]
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 
