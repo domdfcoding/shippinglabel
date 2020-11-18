cd45bc5733a8e32223dc5907b248faf6daf66bb4
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-18 22:09:25 +0000

    Added test for "normalize"

---
+++ tests/test_shippinglabel.py
@@ -0,0 +1,20 @@
+# 3rd party
+import pytest
+
+# this package
+from shippinglabel import normalize
+
+
+@pytest.mark.parametrize(
+		"name, expected",
+		[
+				("foo", "foo"),
+				("bar", "bar"),
+				("baz", "baz"),
+				("baz-extensions", "baz-extensions"),
+				("baz_extensions", "baz-extensions"),
+				("baz.extensions", "baz-extensions"),
+				]
+		)
+def test_normalize(name, expected):
+	assert normalize(name) == expected
