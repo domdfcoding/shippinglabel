5a78c8b9e46d3d425d28fb968f233688c5021a3b
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-20 10:28:19 +0000

    Add new sdist module for parsing sdist filenames.

---
+++ shippinglabel/sdist.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+#
+#  sdist.py
+"""
+Utilities for working with source distributions.
+
+.. versionadded:: 0.9.0
+"""
+#
+#  Copyright © 2021 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+#  OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#  Based on wheel-filename
+#  https://github.com/jwodder/wheel-filename
+#  Copyright (c) 2020 John Thorvald Wodder II
+#  MIT Licensed
+
+# stdlib
+import os
+import re
+from typing import NamedTuple
+
+# 3rd party
+from domdf_python_tools.paths import PathPlus
+from domdf_python_tools.typing import PathLike
+from wheel_filename import InvalidFilenameError
+
+__all__ = ["ParsedSdistFilename", "parse_sdist_filename"]
+
+
+class NotAnSdistError(ValueError):
+	"""
+	Raised when something other than a source distribution is passed to :func:`~.parse_sdist_filename`.
+	"""
+
+	filename: str
+
+	def __init__(self, filename: PathLike, msg: str = '') -> None:
+		#: The invalid filename
+		self.filename = str(filename)
+		super().__init__(msg)
+
+	def __str__(self) -> str:
+		return super().__str__().format(repr(self.filename))
+
+
+class ParsedSdistFilename(NamedTuple):
+	"""
+	Represents a parsed sdist filename.
+
+	:param project: The name of the project.
+	"""
+
+	#: The name of the project. The case is the same as in the filename.
+	project: str
+
+	#: The version number of the project.
+	version: str
+
+	#: The file extension of the sdist, e.g. ``.tar.gz``.
+	extension: str
+
+	def __str__(self) -> str:
+		return f'{self.project}-{self.version}{self.extension}'
+
+
+SDIST_FILENAME_CRGX = re.compile(
+		r'(?P<project>[A-Za-z0-9](?:[A-Za-z0-9._-]*[A-Za-z0-9])?(-stubs)?)'
+		r'-(?P<version>[A-Za-z0-9_.!+]+)'
+		r'(?P<extension>.tar.gz|.tar.bz2|.zip)'
+		)
+
+
+def parse_sdist_filename(filename: PathLike) -> ParsedSdistFilename:
+	"""
+	Parse a sdist filename into its components.
+
+	:param str filename: A sdist path or filename
+	:rtype: ParsedWheelFilename
+	:raises InvalidFilenameError: if the filename is invalid
+	"""
+
+	filename = PathPlus(filename)
+
+	if filename.suffix == ".whl":
+		raise NotAnSdistError(filename, "{} is a wheel.")
+
+	basename = os.path.basename(filename)
+
+	m = SDIST_FILENAME_CRGX.fullmatch(basename)
+	if not m:
+		raise InvalidFilenameError(basename)
+
+	return ParsedSdistFilename(**m.groupdict())
---
+++ tests/test_sdist.py
@@ -0,0 +1,58 @@
+# 3rd party
+import pytest
+from pytest_regressions.data_regression import DataRegressionFixture
+from wheel_filename import InvalidFilenameError
+
+# this package
+from shippinglabel.sdist import NotAnSdistError, parse_sdist_filename
+
+
+@pytest.mark.parametrize(
+		"filename",
+		[
+				"wxPython-4.1.0.tar.gz",
+				"PyGObject-3.38.0.tar.gz",
+				"psutil-5.8.0.tar.gz",
+				"tinycss-0.4.tar.gz",
+				"cffi-1.14.4.tar.gz",
+				"termcolor-1.1.0.tar.gz",
+				"autodocsumm-0.2.2.tar.gz",
+				"MarkupSafe-1.1.1.tar.gz",
+				"astropy-4.2.tar.gz",
+				"pyrsistent-0.17.3.tar.gz",
+				"memoized-property-1.0.3.tar.gz",
+				"PyNaCl-1.4.0.tar.gz",
+				"pycairo-1.20.0.tar.gz",
+				"strict-rfc3339-0.7.tar.gz",
+				"autoflake-1.4.tar.gz",
+				"Pillow-8.1.0.tar.gz",
+				"cairocffi-1.2.0.tar.gz",
+				"quantities-0.12.4.tar.gz",
+				"pyerfa-1.7.1.1.tar.gz",
+				"matplotlib-3.3.3.tar.gz",
+				"fastimport-0.9.8.tar.gz",
+				"orderedset-2.0.3.tar.gz",
+				"PyYAML-5.3.1.tar.gz",
+				"pandas-1.2.0.tar.gz",
+				"Cython-0.29.14.tar.gz",
+				"PyGObject-stubs-0.0.2.tar.gz",
+				"dulwich-0.20.15.tar.gz",
+				"tox-pip-version-0.0.7.tar.gz",
+				"numpy-1.16.5.zip",
+				]
+		)
+def test_parse_sdist_filename(filename: str, data_regression: DataRegressionFixture):
+	parsed_filename = parse_sdist_filename(filename)
+	data_regression.check(parsed_filename._asdict())
+	assert str(parsed_filename) == filename
+
+
+def test_parse_sdist_filename_wheel():
+	# Test passing in a wheel
+	with pytest.raises(NotAnSdistError, match="'autopep8-1.5.4-py2.py3-none-any.whl' is a wheel."):
+		parse_sdist_filename("autopep8-1.5.4-py2.py3-none-any.whl")
+
+
+def test_parse_sdist_filename_invalid():
+	with pytest.raises(InvalidFilenameError, match="Invalid wheel filename: 'autopep8-.tar.bz2'"):
+		parse_sdist_filename("autopep8-.tar.bz2")
